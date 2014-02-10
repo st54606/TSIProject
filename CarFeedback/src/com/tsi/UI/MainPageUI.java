6f5c0199430b57f0a8ce25d7ad91e0e9d93bf784
Vitalij <Vitalij@ASUSN56>
2014-02-10 08:42:12 +0200

    testPush

--- CarFeedback/src/com/tsi/UI/MainPageUI.java
+++ CarFeedback/src/com/tsi/UI/MainPageUI.java
@@ -23,17 +23,7 @@ public class MainPageUI extends UI {
 
 	@Override
 	protected void init(VaadinRequest request) {
-		final VerticalLayout layout = new VerticalLayout();
-		layout.setMargin(true);
-		setContent(layout);
-
-		Button button = new Button("Click Me");
-		button.addClickListener(new Button.ClickListener() {
-			public void buttonClick(ClickEvent event) {
-				layout.addComponent(new Label("Thank you for clicking"));
-			}
-		});
-		layout.addComponent(button);
+		
 	}
 
 }
